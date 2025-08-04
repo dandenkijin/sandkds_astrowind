d73d769d4f272c41a4df83925e741e6d67872571
dandenkijin <dandenkijin@gmail.com>
2025-08-04 03:16:46 -0400

    cleaned up footer

--- src/navigation.ts
+++ src/navigation.ts
@@ -97,85 +97,87 @@ export const headerData = {
     //     },
     //   ],
     // },
-   // {
-   //    text: 'Blog',
-   //    links: [
-   //      {
-   //        text: 'Blog List',
-   //        href: getBlogPermalink(),
-   //      },
-   //      {
-   //        text: 'Article',
-   //        href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-   //      },
-   //      {
-   //        text: 'Article (with MDX)',
-   //        href: getPermalink('markdown-elements-demo-post', 'post'),
-   //      },
-   //      {
-   //        text: 'Category Page',
-   //        href: getPermalink('tutorials', 'category'),
-   //      },
-   //      {
-   //        text: 'Tag Page',
-   //        href: getPermalink('astro', 'tag'),
-   //      },
-   //    ],
-   // },
+    // {
+    //    text: 'Blog',
+    //    links: [
+    //      {
+    //        text: 'Blog List',
+    //        href: getBlogPermalink(),
+    //      },
+    //      {
+    //        text: 'Article',
+    //        href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //      },
+    //      {
+    //        text: 'Article (with MDX)',
+    //        href: getPermalink('markdown-elements-demo-post', 'post'),
+    //      },
+    //      {
+    //        text: 'Category Page',
+    //        href: getPermalink('tutorials', 'category'),
+    //      },
+    //      {
+    //        text: 'Tag Page',
+    //        href: getPermalink('astro', 'tag'),
+    //      },
+    //    ],
+    // },
   ],
-  actions: [{ text: 'Contact Us', href: getPermalink('/justle') }],
+  actions: [
+    //{ text: 'Contact Us', href: getPermalink('/justle') }
+  ],
 };
 
 export const footerData = {
   links: [
-    {
-      title: 'Product',
-      links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
-        { text: 'Resources', href: '#' },
-      ],
-    },
-    {
-      title: 'Platform',
-      links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
-      ],
-    },
+    //{
+    //  title: 'Product',
+    //  links: [
+    //    { text: 'Features', href: '#' },
+    //    { text: 'Security', href: '#' },
+    //    { text: 'Team', href: '#' },
+    //    { text: 'Enterprise', href: '#' },
+    //    { text: 'Customer stories', href: '#' },
+    //    { text: 'Pricing', href: '#' },
+    //    { text: 'Resources', href: '#' },
+    //  ],
+    //},
+    //{
+    //  title: 'Platform',
+    //  links: [
+    //    { text: 'Developer API', href: '#' },
+    //    { text: 'Partners', href: '#' },
+    //    { text: 'Atom', href: '#' },
+    //    { text: 'Electron', href: '#' },
+    //    { text: 'AstroWind Desktop', href: '#' },
+    //  ],
+    //},
+    //{
+    //  title: 'Support',
+    //  links: [
+    //    { text: 'Docs', href: '#' },
+    //    { text: 'Community Forum', href: '#' },
+    //    { text: 'Professional Services', href: '#' },
+    //    { text: 'Skills', href: '#' },
+    //    { text: 'Status', href: '#' },
+    //  ],
+    //},
+    //{
+    //  title: 'Company',
+    //  links: [
+    //    { text: 'About', href: '#' },
+    //    { text: 'Blog', href: '#' },
+    //    { text: 'Careers', href: '#' },
+    //    { text: 'Press', href: '#' },
+    //    { text: 'Inclusion', href: '#' },
+    //    { text: 'Social Impact', href: '#' },
+    //    { text: 'Shop', href: '#' },
+    //  ],
+    //},
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    //{ text: 'Terms', href: getPermalink('/terms') },
+    //{ text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
